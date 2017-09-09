c195243aeceae756b604f8e1858845c652587f15
zengls <syslock2011@gmail.com>
2017-09-09 17:53:39 +0800

    u

--- scrapy_tt/scrapy_tt/spiders/csdn_spider.py
+++ scrapy_tt/scrapy_tt/spiders/csdn_spider.py
@@ -4,7 +4,7 @@ from scrapy_tt.items import CSDNItem
 class CSDNSpider(scrapy.Spider):
 	name = 'csdn'
 	allowed_domains = ['csdn.net']
-	start_urls = ["http://geek.csdn.net/bigdata","http://geek.csdn.net/iot"]
+	start_urls = ["http://geek.csdn.net/bigdata"]
 
 	def parse(self,response):
 
@@ -13,9 +13,15 @@ class CSDNSpider(scrapy.Spider):
 		# 	f.write(response.body)
 		item = CSDNItem()
 		geek_lists = response.xpath('//dl[@class=\'geek_list\']/dd')
+		
 		for sel in geek_lists:
 			item['title'] = sel.xpath('span/a/text()').extract()
 			item['link'] = sel.xpath('span/a/@href').extract()
 			item['reading_num'] = sel.xpath('ul/li[@class=\'read_num\']/em/text()').extract()
 			item['date'] = sel.xpath('ul/li[2]/text()').extract()
-			yield item
+			# yield item
+		# with open('E:\\news.txt','w') as f:
+			# for i in item:
+			# 	f.write(i)
+		print(item['title'])
+	
